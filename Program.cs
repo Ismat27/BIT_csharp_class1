dabb5dc0973551ec9ab4cfbe578482c5a34fc371
ismat27 <adegbiteismail27@gmail.com>
2023-11-22 09:10:13 +0100

    Update area calculation

--- Program.cs
+++ Program.cs
@@ -26,47 +26,71 @@ namespace Wema.BIT.Program
             foreach (User user in users)
             {
                 Random random = new Random();
-                decimal baseAmount = 100;
                 for (int i = 0; i < 3; i++)
                 {
                     var paymentId = random.Next();
-                    Payment payment = user.makePayement(baseAmount * (1 + i));
+                    Payment payment = user.makePayement(random.Next(5000));
                     payment.Id = paymentId;
                     payment.User = user;
                     user.Payments.Add(payment);
                     payments.Add(payment);
                 }
             }
-            //foreach (Payment payment in payments)
-            //{
-            //    Console.WriteLine(payment.formatDetails());
-            //}
+            foreach (Payment payment in payments)
+            {
+                Console.WriteLine(payment.formatDetails());
+            }
 
-            // Triangle Asignment
-            Console.WriteLine("\n\nTriangle Assignment\n");
+            //Triangle Asignment
+            Console.WriteLine("\nCalcualte Triangle Area\n");
+            Console.WriteLine("Option 1: Using all three sides\nOption 2: Using base and height");
+            Console.WriteLine("For option enter 1, for option 2 enter 2 or any key");
+            var userInput = Console.ReadLine();
+            double area;
+            if (userInput == "1")
+            {
+                Console.WriteLine("Enter length of side one: ");
+                var sideOne = Console.ReadLine();
+                if (!double.TryParse(sideOne, out double a))
+                {
+                    Console.WriteLine("Invalid value entered. Enter number only");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Enter length of side two: ");
+                var sideTwo = Console.ReadLine();
+                if (!double.TryParse(sideTwo, out double b))
+                {
+                    Console.WriteLine("Invalid value entered. Enter number only");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Enter length of side three: ");
+                var sideThree = Console.ReadLine();
+                if (!double.TryParse(sideThree, out double c))
+                {
+                    Console.WriteLine("Invalid value entered. Enter number only");
+                    Environment.Exit(1);
+                }
+                area = Triangle.AreaFromThreeSides(a, b, c);
+            }
 
-            Console.WriteLine("Enter length of side one: ");
-            var sideOne = Console.ReadLine();
-            if (!double.TryParse(sideOne, out double a))
+            else
             {
-                Console.WriteLine("Invalid value entered. Enter number only");
-                Environment.Exit(1);
+                Console.WriteLine("Enter length of the base: ");
+                var sideOne = Console.ReadLine();
+                if (!double.TryParse(sideOne, out double Base))
+                {
+                    Console.WriteLine("Invalid value entered. Enter number only");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Enter the height: ");
+                var sideTwo = Console.ReadLine();
+                if (!double.TryParse(sideTwo, out double Height))
+                {
+                    Console.WriteLine("Invalid value entered. Enter number only");
+                    Environment.Exit(1);
+                }
+                area = Triangle.AreaFromBaseHeight(Base, Height);
             }
-            Console.WriteLine("Enter length of side two: ");
-            var sideTwo = Console.ReadLine();
-            if (!double.TryParse(sideTwo, out double b))
-            {
-                Console.WriteLine("Invalid value entered. Enter number only");
-                Environment.Exit(1);
-            }
-            Console.WriteLine("Enter length of side three: ");
-            var sideThree = Console.ReadLine();
-            if (!double.TryParse(sideThree, out double c))
-            {
-                Console.WriteLine("Invalid value entered. Enter number only");
-                Environment.Exit(1);
-            }
-            var area = Triangle.AreaFromThreeSides(a, b, c);
             Console.WriteLine($"The triangle area is {area}");
         }
     }
